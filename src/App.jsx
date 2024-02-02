6730582b5d1a5e9682c04089cda3c0ec6cb0e634
Ricardo_Li <46476463+RicardoYam@users.noreply.github.com>
2024-02-02 16:42:52 +1000

    Can save to localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import AddTask from './components/AddTask'
 import Todolist from './components/Todolist'
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [todoList, setTodoList] = useState([])
+  const TODOLIST_KEY = 'todoList'
+  const [todoList, setTodoList] = useState(() => {
+     // Use a function to initialize the state to ensure this code only runs once
+     const storedTodoList = localStorage.getItem(TODOLIST_KEY);
+     // Parse the stored list if it exists, otherwise default to an empty array
+     return storedTodoList ? JSON.parse(storedTodoList) : [];
+  })
+
+  useEffect(() => {
+    localStorage.setItem(TODOLIST_KEY, JSON.stringify(todoList))
+  }, [todoList])
 
   const addTask = (task) => {
-    setTodoList([...todoList, {id: uuidv4(), name: task, comleted: false}])
+    setTodoList([...todoList, {id: uuidv4(), name: task, completed: false}])
   }
+
   
   return (
     <>
